3ab086be1d9253f01f918c2c3eaa94cb2ae76db2
andormade <hngrhorace@gmail.com>
2021-04-05 11:48:48 +0000

    feat: add google tag

--- _build/render/renderHtml.js
+++ _build/render/renderHtml.js
@@ -13,6 +13,14 @@ module.exports = function (Component, props) {
 			<!DOCTYPE html>
 				<html ${helmet.htmlAttributes.toString()}>
 				<head>
+					<!-- Global site tag (gtag.js) - Google Analytics -->
+					<script async src="https://www.googletagmanager.com/gtag/js?id=UA-82349820-1"></script>
+					<script>
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', 'UA-82349820-1');
+					</script>
 					${helmet.title.toString()}
 					${helmet.meta.toString()}
 					${helmet.link.toString()}
