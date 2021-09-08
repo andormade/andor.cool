8e373d23ee53ece12410421b142fd6daee09ec2d
andormade <hngrhorace@gmail.com>
2021-09-08 21:32:28 +0000

    change text color

--- _layouts/ColorScheme.jsx
+++ _layouts/ColorScheme.jsx
@@ -4,8 +4,8 @@ export default createGlobalStyle`
     :root {
         --background-color: #fff0ee;
         --font-size: 18px;
-        --text-color: #999999;
-        --title-color: #cccccc;
+        --text-color: #333399;
+        --title-color: #333399;
 
         // @media (prefers-color-scheme: dark) {
         //     --background-color: #111111;
