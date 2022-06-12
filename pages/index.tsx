b0840f787c35fc7b2b9a28d16f11633ce07964be
andormade <hngrhorace@gmail.com>
2022-06-12 14:16:10 +0000

    feat: indicate years in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,27 +3,48 @@ import { GetStaticProps, GetStaticPropsResult } from 'next';
 import Head from 'next/head';
 import { collectPosts } from '../scripts/collectPosts';
 
+type PostProps = {
+	slug: string;
+	title: string;
+	emojis?: string;
+	date: string;
+};
+
 interface HomeProps {
-	posts: {
-		slug: string;
-		title: string;
-		emojis?: string;
-	}[];
+	posts: PostProps[];
+}
+
+function groupPostsByYear(posts: PostProps[]) {
+	return posts.reduce((previous, current) => {
+		const year = new Date(current.date).getFullYear().toString();
+		if (!previous[year]) {
+			previous[year] = [];
+		}
+		previous[year].push(current) 
+		return previous
+	}, {} as Record<string, PostProps[]>);
 }
 
 const Home: NextPage<HomeProps> = ({ posts }) => {
+	const groupsByYear = groupPostsByYear(posts);
+
 	return (
 		<div>
 			<Head>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<ul className="postlist">
-				{posts.map((post, index) => {
-					return (
-						<li key={index}>
-							<a href={`/posts/${post.slug}`}>{post.title}</a> {post.emojis}
-						</li>
-					);
+				{Object.keys(groupsByYear).reverse().map((year) => {
+					return groupsByYear[year].map((post, index) => {
+						return (
+							<>
+								{ index === 0 && <span className="date">{year}</span>}
+								<li key={index}>
+									<a href={`/posts/${post.slug}`}>{post.title}</a> {post.emojis}
+								</li>
+							</>
+						);
+					})
 				})}
 			</ul>
 		</div>
@@ -39,6 +60,7 @@ export const getStaticProps: GetStaticProps = async function (): Promise<GetStat
 				title: post.attributes.title,
 				slug: post.slug,
 				emojis: post.attributes.emojis || '',
+				date: post.attributes.date
 			})),
 		},
 	};
