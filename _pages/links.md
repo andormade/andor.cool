1abb0751a8a850d3a6a831ea199db0c8c711c54f
andormade <hngrhorace@gmail.com>
2022-10-28 18:13:34 +0000

    add links page

---
+++ _pages/links.md
@@ -0,0 +1,17 @@
+---
+layout: post
+title: 'Links'
+date: 2022-10-28 19:00:00 -0100
+location: 'Zandvoort, Netherlands'
+---
+
+Links to different public online profiles that belong to me:
+
+- <a href="https://github.com/andormade">GitHub</a>
+- <a href="https://instagram.com/andorcover">Instagram</a>
+- <a href="https://www.youtube.com/c/Polg%C3%A1rAndor">YouTube</a>
+- <a href="https://nl.linkedin.com/in/andormade">LinkedIn</a>
+- <a href="https://www.imdb.com/name/nm11601357/">IMDb<a>
+- <a href="https://open.spotify.com/user/hngrhorace">Spotify</a>
+- <a href="https://twitter.com/andormade">Twitter</a>
+- <a href="https://www.flickr.com/photos/hngrhorace/">Flickr</a>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,14 +7,14 @@ function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<div className="container">
 			<Head>
-				<title>Andor Polgar&apos;s photo journal</title>
+				<title>Andor Polgar&apos;s visual journal</title>
 				<meta
 					name="viewport"
 					content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no"
 				/>
 			</Head>
 			<h1>
-				<Link href="/">Andor Polgar&apos;s photo journal</Link>
+				<Link href="/">Andor Polgar&apos;s visual journal</Link>
 			</h1>
 			<div>
 				<Component {...pageProps} />
@@ -22,10 +22,11 @@ function MyApp({ Component, pageProps }: AppProps) {
 			<footer>
 				<p>
 					Instagram: <Link href="https://instagram.com/andorcover">@andorcover</Link> |{' '}
-					<Link href="https://www.youtube.com/channel/UCF_EgDIkYFIeu-19KleLnFA">YouTube</Link> | My website doesn&apos;t
-					collect your personal information, nor does it send any of it to third-party analytics services. 🍪 You
-					don&apos;t have to believe me, you can see it for yourself in the source code on{' '}
-					<Link href="https://github.com/andormade/andor.cool">github</Link>.
+					<Link href="https://www.youtube.com/channel/UCF_EgDIkYFIeu-19KleLnFA">YouTube</Link> | {' '}
+					<Link href="/links">Other links</Link> |{' '}
+					My website doesn&apos;t collect your personal information, nor does it send any of it to
+					third-party analytics services. 🍪 You don&apos;t have to believe me, you can see it for yourself in
+					the source code on <Link href="https://github.com/andormade/andor.cool">github</Link>.
 				</p>
 			</footer>
 		</div>
