e109d99891def6a2d817e4155ac13a5cbf6e5ab9
andormade <hngrhorace@gmail.com>
2024-03-09 22:07:17 +0000

    chore: refactoring

--- rust/src/load_includes.rs
+++ rust/src/load_includes.rs
@@ -1,22 +1,17 @@
 use std::collections::HashMap;
 use std::fs;
-use std::io::Result;
 use std::path::Path;
 
-pub fn load_liquid_includes() -> Result<HashMap<String, String>> {
-    let path = Path::new("../_includes");
+pub fn load_liquid_includes(dir_path: &str) -> HashMap<String, String> {
+    let path = Path::new(dir_path);
     let mut templates = HashMap::new();
 
-    for entry in fs::read_dir(path)? {
-        let entry = entry?;
-        let path = entry.path();
-
-        if path.is_file() {
-            if let Some(extension) = path.extension().and_then(|ext| ext.to_str()) {
-                if extension == "liquid" {
-                    if let Some(filename) = path.file_name().and_then(|name| name.to_str()) {
-                        let path_clone = path.clone();
-                        let contents = fs::read_to_string(path_clone)?;
+    if let Ok(entries) = fs::read_dir(path) {
+        for entry in entries.flatten() {
+            let path = entry.path();
+            if path.is_file() && path.extension().and_then(|ext| ext.to_str()) == Some("liquid") {
+                if let Some(filename) = path.file_stem().and_then(|name| name.to_str()) {
+                    if let Ok(contents) = fs::read_to_string(&path) {
                         templates.insert(filename.to_string(), contents);
                     }
                 }
@@ -24,7 +19,7 @@ pub fn load_liquid_includes() -> Result<HashMap<String, String>> {
         }
     }
 
-    Ok(templates)
+    templates
 }
 
 #[cfg(test)]
@@ -36,7 +31,7 @@ mod tests {
 
     #[test]
     fn test_load_liquid_includes() {
-        let templates = load_liquid_includes().unwrap();
+        let templates = load_liquid_includes("../_includes");
         let sorted_templates: BTreeMap<_, _> = templates.into_iter().collect();
         let templates_json = serde_json::to_string_pretty(&sorted_templates).unwrap();
         // Use assert_snapshot! to compare the output against the stored snapshot
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,7 @@
 use std::io::Result;
 use crate::file_readers::load_and_parse_markdown_files_with_front_matter_in_directory;
 use crate::handlebars::replace_template_variables;
+use crate::load_includes::load_liquid_includes;
 
 mod front_matter;
 mod handlebars;
@@ -11,13 +12,15 @@ mod markdown_with_front_matter;
 mod file_readers;
 
 fn main() -> Result<()> {
-    let markdown_files = load_and_parse_markdown_files_with_front_matter_in_directory("../_posts")?;
+    let posts = load_and_parse_markdown_files_with_front_matter_in_directory("../_posts")?;
+    let includes = load_liquid_includes("../_includes");
+    let list_item_template = includes.get("list_item").cloned().unwrap_or_else(String::new);
 
     let mut html_list = String::new();
     html_list.push_str("<ul>\n");
 
-    for markdown in markdown_files {
-        html_list.push_str(&replace_template_variables("<li><a href=\"{{slug}}\">{{title}}</a><li>\n", &markdown ));
+    for post in posts {
+        html_list.push_str(&replace_template_variables(&list_item_template, &post ));
     }
 
     html_list.push_str("</ul>");
