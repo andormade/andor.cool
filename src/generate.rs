e22382d2aba56a26428029242c42e81813b5464b
andormade <hngrhorace@gmail.com>
2025-06-02 22:00:39 +0000

    chore: refactor generate function

--- src/generate.rs
+++ src/generate.rs
@@ -41,36 +41,14 @@ fn render_page(
     Ok(())
 }
 
-pub fn generate(site_name: &str) -> Result<()> {
-    // Get the current system time
-    let now = SystemTime::now();
-    let duration_since_epoch = now.duration_since(UNIX_EPOCH).expect("Time went backwards");
-    let generated_date = duration_since_epoch.as_secs().to_string();
-
-    let css_file_name = copy_file_with_versioning(&format!("./sites/{}/style.css", site_name), "./out/")?;
-    let posts = load_and_parse_markdown_files_with_front_matter_in_directory(&format!("./sites/{}/posts", site_name))?;
-    let pages = load_and_parse_markdown_files_with_front_matter_in_directory(&format!("./sites/{}/pages", site_name))?;
-    let includes = load_liquid_includes(&format!("./sites/{}/includes", site_name));
-
-    let mut global_variables = HashMap::new();
-    global_variables.insert(
-        "title".to_string(),
-        "Andor Polgar's Visual Journal".to_string(),
-    );
-
-    let main_layout_template = load_layout(&format!("./sites/{}/layouts/main.html", site_name))?;
-    let mut main_layout_variables = HashMap::new();
-    main_layout_variables.insert("css_file_name".to_string(), css_file_name);
-    main_layout_variables.insert("generated_date".to_string(), generated_date);
-    let mut main_layout = replace_template_variables(&main_layout_template, &main_layout_variables);
-
-    generate_pagination_pages(5, &posts, &includes, &main_layout, &global_variables);
-
-    // Generate index page
-
+fn generate_index_page(
+    posts: &Vec<HashMap<String, String>>,
+    includes: &HashMap<String, String>,
+    main_layout: &str,
+) -> Result<()> {
     // Group posts by year
     let mut posts_by_year: HashMap<String, Vec<HashMap<String, String>>> = HashMap::new();
-    for post in &posts {
+    for post in posts {
         if let Some(date_str) = post.get("date") {
             let year = &date_str[0..4]; // Extract the first 4 characters as the year
             posts_by_year
@@ -113,8 +91,17 @@ pub fn generate(site_name: &str) -> Result<()> {
     html = remove_handlebars_variables(&html)?;
     write_html_to_file(&"out/index.html", &html)?;
 
-    // Generate posts
-    for post in &posts {
+    Ok(())
+}
+
+fn generate_posts(
+    posts: &Vec<HashMap<String, String>>,
+    includes: &HashMap<String, String>,
+    main_layout_template: &str,
+    main_layout_variables: &mut HashMap<String, String>,
+    global_variables: &mut HashMap<String, String>,
+) -> Result<()> {
+    for post in posts {
         global_variables.insert(
             "title".to_string(),
             post.get("title")
@@ -147,9 +134,17 @@ pub fn generate(site_name: &str) -> Result<()> {
             &global_variables,
         )?;
     }
+    Ok(())
+}
 
-    // Generate pages
-    for page in &pages {
+fn generate_pages(
+    pages: &Vec<HashMap<String, String>>,
+    includes: &HashMap<String, String>,
+    main_layout_template: &str,
+    main_layout_variables: &mut HashMap<String, String>,
+    global_variables: &mut HashMap<String, String>,
+) -> Result<()> {
+    for page in pages {
         global_variables.insert(
             "title".to_string(),
             page.get("title")
@@ -162,7 +157,7 @@ pub fn generate(site_name: &str) -> Result<()> {
 
         let slug = page.get("slug").cloned().unwrap_or_else(String::new);
         main_layout_variables.insert("pathname".to_string(), slug);
-        main_layout = replace_template_variables(&main_layout_template, &main_layout_variables);
+        let main_layout = replace_template_variables(&main_layout_template, &main_layout_variables);
 
         render_page(
             &page.get("content").map(|s| s.as_str()).unwrap_or(""),
@@ -173,6 +168,54 @@ pub fn generate(site_name: &str) -> Result<()> {
             &global_variables,
         )?;
     }
+    Ok(())
+}
+
+pub fn generate(site_name: &str) -> Result<()> {
+    // Get the current system time
+    let now = SystemTime::now();
+    let duration_since_epoch = now.duration_since(UNIX_EPOCH).expect("Time went backwards");
+    let generated_date = duration_since_epoch.as_secs().to_string();
+
+    let css_file_name = copy_file_with_versioning(&format!("./sites/{}/style.css", site_name), "./out/")?;
+    let posts = load_and_parse_markdown_files_with_front_matter_in_directory(&format!("./sites/{}/posts", site_name))?;
+    let pages = load_and_parse_markdown_files_with_front_matter_in_directory(&format!("./sites/{}/pages", site_name))?;
+    let includes = load_liquid_includes(&format!("./sites/{}/includes", site_name));
+
+    let mut global_variables = HashMap::new();
+    global_variables.insert(
+        "title".to_string(),
+        "Andor Polgar's Visual Journal".to_string(),
+    );
+
+    let main_layout_template = load_layout(&format!("./sites/{}/layouts/main.html", site_name))?;
+    let mut main_layout_variables = HashMap::new();
+    main_layout_variables.insert("css_file_name".to_string(), css_file_name);
+    main_layout_variables.insert("generated_date".to_string(), generated_date);
+    let main_layout = replace_template_variables(&main_layout_template, &main_layout_variables);
+
+    generate_pagination_pages(5, &posts, &includes, &main_layout, &global_variables);
+
+    // Generate index page
+    generate_index_page(&posts, &includes, &main_layout)?;
+
+    // Generate posts
+    generate_posts(
+        &posts,
+        &includes,
+        &main_layout_template,
+        &mut main_layout_variables,
+        &mut global_variables,
+    )?;
+
+    // Generate pages
+    generate_pages(
+        &pages,
+        &includes,
+        &main_layout_template,
+        &mut main_layout_variables,
+        &mut global_variables,
+    )?;
 
     Ok(())
 }
